e84421d820ec57514553b567c69a1924432bfa41
YusukeIGR <javakunren84@gmail.com>
2023-02-09 12:07:48 +0900

    ~P.217

---
+++ Assets/Script/CandyDestroyer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+   void OnTriggerEnter(Collider other){
+    if(other.gameObject.tag=="Candy"){
+        //オブジェクトを削除
+        Destroy(other.gameObject);
+    }
+   }
+}
