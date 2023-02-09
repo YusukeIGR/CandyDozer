724567b796262f78a11dd671e47a7e85b55312f2
YusukeIGR <javakunren84@gmail.com>
2023-02-09 13:25:46 +0900

    ~P.224

--- Assets/Script/CandyDestroyer.cs
+++ Assets/Script/CandyDestroyer.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class CandyDestroyer : MonoBehaviour
 {
+    public CandyManager candyManager;
+    public int reward;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
    void OnTriggerEnter(Collider other){
     if(other.gameObject.tag=="Candy"){
         //オブジェクトを削除
         Destroy(other.gameObject);
+
+        if(effectPrefab!=null){
+            //Candyのポジションにエフェクトを作成
+            Instantiate(
+                effectPrefab,
+                other.transform.position,
+                Quaternion.Euler(effectRotation)
+            );
+        }
     }
    }
 }
